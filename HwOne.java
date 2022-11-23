86ff8996944cbcf09d104995ad35867f4b18a9bb
zhukovv <northbug@yandex.ru>
2022-11-23 20:50:36 +0500

    solved second task

--- HwOne.java
+++ HwOne.java
@@ -3,40 +3,47 @@ import java.util.Scanner;
 public class HwOne {
     public static void main(String[] args) {
 //        firstTask();
-        secondTask(10);
+        secondTask(100);
     }
-    static void firstTask(){
+
+    static void firstTask() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Хочу треугольное число, пиши :  ");
         int num = scan.nextInt();
-        num ++;
+        num++;
         int sum = 0;
         int mult = 1;
         for (int i = 0; i < num; i++) {
             sum = sum + i;
             if (i > 0) {
                 mult = mult * i;
-            };
-    }
+            }
+            ;
+        }
         System.out.println("Сумма равна " + sum + "; Произведение равно " + mult);
     }
 
-    static void secondTask(int num){
-        for (int i = 2; i < num; i++) {
-            Boolean simple = True;
-            for (int j = 2; j < (i-1); j++) {
-                if (i % j == 0) {
-                    simple = False;
-                }
-            if (simple){
+    static void secondTask(int num) {
+        for (int i = 2; i <= num - 1; i++) {
+            if(check(i)){
                 System.out.println(i);
             }
 
+
+
         }
 
+
     }
-
-
+    static boolean check(int num){
+        if (num == 2){return true;}
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 
