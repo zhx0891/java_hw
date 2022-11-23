ef4885c3507156e4e6881a687bccc14a2cf1d5be
zhukovv <northbug@yandex.ru>
2022-11-23 12:55:02 +0500

    initiate first task

---
+++ HwOne.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HwOne {
+    public static void main(String[] args) {
+        firstTask();
+
+
+
+
+
+
+
+    }
+    static void firstTask(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("???");
+        int num = scan.nextInt();
+        num ++;
+        System.out.println(num);
+
+    }
+
+
+}
