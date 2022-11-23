5a7f2df4f62b7ce3e4278ee62cb006f1c808c539
zhukovv <northbug@yandex.ru>
2022-11-23 21:35:49 +0500

    third task

--- HwOne.java
+++ HwOne.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class HwOne {
     public static void main(String[] args) {
 //        firstTask();
-        secondTask(100);
+//        secondTask(100);
+        thirdTask();
     }
+
 
     static void firstTask() {
         Scanner scan = new Scanner(System.in);
@@ -18,32 +20,60 @@ public class HwOne {
             if (i > 0) {
                 mult = mult * i;
             }
-            ;
         }
         System.out.println("Сумма равна " + sum + "; Произведение равно " + mult);
     }
 
+
     static void secondTask(int num) {
         for (int i = 2; i <= num - 1; i++) {
-            if(check(i)){
+            if (check(i)) {
                 System.out.println(i);
             }
-
-
-
         }
-
-
     }
-    static boolean check(int num){
-        if (num == 2){return true;}
+
+
+    static boolean check(int num) {
+        if (num == 2) {
+            return true;
+        }
         for (int i = 2; i < num; i++) {
-            if(num % i == 0){
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
     }
-}
 
+    static void thirdTask() {
+        System.out.println("Введите выражение вида 'int' 'действие' 'int'\nнапример  2 + 2  между символами обязателен пробел\nДоступные операции - сложение, умножение, деление, произведение ");
+        Scanner str = new Scanner(System.in);
+        String expr = str.nextLine();
+        String[] strSplit = expr.split(" ");
+        if (strSplit.length != 3) {System.out.println("Некорректный ввод");}
+        else {
+            int  a = Integer.parseInt(strSplit[0]);
+            int  b = Integer.parseInt(strSplit[2]);
+            switch (strSplit[1]){
+                case ("+"):
+                    System.out.println(expr  + " = " + (a+b));
+                    break;
+                case ("-"):
+                    System.out.println(expr  + " = " + (a-b));
+                    break;
+                case ("*"):
+                    System.out.println(expr  + " = " + (a*b));
+                    break;
+                case ("/"):
+                    System.out.println(expr  + " = " + (a/b));
+                    break;
+                default:
+                    System.out.println("что то пошло не так  ;) ");
+
+            }
+
+        }
+    }
+}
 
