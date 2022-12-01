b27b55994ce25a099a622930b0784b44c116547f
zhukovv <northbug@yandex.ru>
2022-12-01 14:47:35 +0500

    второе задание семинара

--- hwTwo.java
+++ hwTwo.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class hwTwo {
     public static void main(String[] args) {
-        firstTaskFromSeminar();
+//        firstTaskFromSeminar();
+        secondTaskFromSeminar("aaabbbcccddd");
+
 
 
     }
@@ -21,6 +23,23 @@ public class hwTwo {
         }
         System.out.println(result);
 
+
+    }
+    static void secondTaskFromSeminar(String line){
+        int count = 0;
+        StringBuilder result = new StringBuilder();
+        String tmp = String.valueOf(line.charAt(0));
+        for (int i = 0; i < line.length(); i++) {
+            if (String.valueOf(line.charAt(i)).equals(tmp)){
+                count++;
+            }else{
+                result.append(count);
+                result.append(tmp);
+                count = 1;
+                tmp = String.valueOf(line.charAt(i));
+            }
+        }
+        System.out.println(result);
 
     }
 }
