cc68fe97715a2d9444b5d88dfb47ec67d6844934
zhukovv <northbug@yandex.ru>
2023-01-30 14:46:23 +0500

    clean

--- hwTwo.java
+++ hwTwo.java
@@ -6,21 +6,13 @@ import java.util.Scanner;
 
 public class hwTwo {
     public static void main(String[] args) throws IOException {
-        //задачи с семинара
-//        firstTaskFromSeminar();
-////        secondTaskFromSeminar("aaabbbcccddd");
-////        System.out.println(thirdTaskFromSeminar("dfasdfafdsafd"));
-////        fourthTaskFromSeminar();
-////        fifthTaskFromSeminar();
-        int [] firstArr = {3, 5, 6, 8, 0};
+
+        int[] firstArr = {3, 5, 6, 8, 0};
         firstTaskHw(firstArr);
 //        secondTask();
-//
-//
-//
-//
-//
+
     }
+
     static void firstTaskHw(int[] arr) {
         int tmp = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -30,10 +22,11 @@ public class hwTwo {
                     arr[j] = arr[i];
                     arr[i] = tmp;
                 }
-                try (FileWriter writer = new FileWriter("logFirstTask.txt")){
-                writer.append(Arrays.toString(arr) + '\n');
-                writer.flush();
-                }catch (Exception exception){}
+                try (FileWriter writer = new FileWriter("logFirstTask.txt")) {
+                    writer.append(Arrays.toString(arr) + '\n');
+                    writer.flush();
+                } catch (Exception exception) {
+                }
 
             }
         }
@@ -72,100 +65,11 @@ public class hwTwo {
         }
     }
 
-//    static void firstTaskFromSeminar(){
-//        Scanner userInp = new Scanner(System.in);
-//        StringBuilder result = new StringBuilder();
-//        int a = 1;
-//        int b = 0;
-//        int c = userInp.nextInt();
-//        int count = 1;
-//        for (int i = 0; i < c ; i++) {
-//            if (count % 2 != 0){
-//                result.append(a);
-//            }else {result.append(b);}
-//            count++;
-//        }
-//        System.out.println(result);
-//
-//
-//    }
 
 
-//    static void secondTaskFromSeminar(String line){
-//        int count = 0;
-//        StringBuilder result = new StringBuilder();
-//        String tmp = String.valueOf(line.charAt(0));
-//        for (int i = 0; i < line.length(); i++) {
-//            if (String.valueOf(line.charAt(i)).equals(tmp)){
-//                count++;
-//            }else{
-//                result.append(count);
-//                result.append(tmp);
-//                count = 1;
-//                tmp = String.valueOf(line.charAt(i));
-//            }
-//        }
-//        System.out.println(result);
-//
-//    }
-//    static boolean thirdTaskFromSeminar(String line){
-//
-//        int cot = 0;
-//        for (int i = 0; i < (line.length() / 2) ; i++) {
-//            cot++;
-//
-//            if(String.valueOf(line.charAt(i)).equals(String.valueOf(line.charAt(line.length() - cot)))){
-//                continue;
-//            }
-//            else{return false;}
-//
-//        }
-//        return true;
-//    }
-//
-//
-//    static void fourthTaskFromSeminar() throws IOException {
-//        StringBuilder result = new StringBuilder();
-//        for (int i = 0; i <= 100 ; i++) {
-//            result.append(i +":"+"test"+" ");
-//        }
-//        System.out.println(result);
-//        FileWriter fw = new FileWriter("100test.txt");
-////        String toFile = result.toString();
-//        fw.write(result.toString());//toString() стрингбилдер в строку
-//        fw.close();
-//
-//    }
-//
-//
-//    static void fifthTaskFromSeminar() throws IOException {
-//        String [] listDir = getListDir();
-//        for (String str : listDir)
-//        {
-//            System.out.println(str);
-//        }
-//
-////        File directory = new File("");
-////        for(File file : directory.listFiles())
-////        {
-////            System.out.println(file.getName());
-////        }
-//
-//    }
-//    static String[] getListDir(){
-//     File directory = new File("");
-//     String path = directory.getAbsolutePath();
-//     File curDir = new File(path);
-////     String [] dir = new String[];
-//     int count = 0;
-//     for (File fi : curDir.listFiles())
-//     {
-//         dir[count] = fi.getName();
-//         count++;
-//     }
-//        return dir;
-//
 }
+
+
 //import java.io.FileWriter;
 //        import java.io.IOException;
 //        import java.util.logging.Logger;
