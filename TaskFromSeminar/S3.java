8d70a8d12bab19eedcaa9adc923e378212fa406a
zhukovv <northbug@yandex.ru>
2023-02-01 13:04:16 +0500

    first task from seminar

--- TaskFromSeminar/S3.java
+++ TaskFromSeminar/S3.java
@@ -3,11 +3,14 @@ package TaskFromSeminar;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class S3 {
     public static void main(String[] args) {
-        taskOne(10);
+//        taskOne(10);
+        taskTwo();
+
     }
 
     static void taskOne(int len){
@@ -19,9 +22,28 @@ public class S3 {
             taskList.add(rand.nextInt(10) );
         }
         Collections.sort(taskList);
-        for (Integer i:taskList){
-            System.out.print(i + " ");
-        }
+        showMeList(taskList);
+//        for (Integer i:taskList){
+//            System.out.print(i + " ");
+//        }
 
+    }
+
+    static void taskTwo(){
+        String [] planets = {"Юпитер", "Марс", "Земля", "Венера", "Меркурий", "Плутон", "Нептун", "Уран", "Сатурн" };
+        Random planet = new Random();
+        ArrayList <String> listPlanets = new ArrayList<>();
+        for (int i = 0; i <planets.length ; i++) {
+            listPlanets.add(planets[planet.nextInt(10)]);
+        }
+        showMeList(listPlanets);
+
+    }
+
+    static void showMeList(List lst){
+        for (int i = 0; i < lst.size(); i++) {
+
+            System.out.print(lst.get(i) + " ");
+        }
     }
 }
