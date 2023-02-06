bbdd5d0c4489f0d8c5594fe4530c8fcdca6e706f
zhukovv <northbug@yandex.ru>
2023-02-06 18:00:07 +0500

    second task in progress

--- HW5/HwFive.java
+++ HW5/HwFive.java
@@ -1,13 +1,48 @@
 package HW5;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class HwFive {
     public static void main(String[] args) {
 // Реализуйте структуру телефонной книги с помощью HashMap,
 // учитывая, что 1 человек может иметь несколько телефонов.
         HashMap<Integer, String> phoneBook = new HashMap<>();
+        String employees = "Иван Иванов\n" +
+                "\n" +
+                "Светлана Петрова\n" +
+                "\n" +
+                "Кристина Белова\n" +
+                "\n" +
+                "Анна Мусина\n" +
+                "\n" +
+                "Анна Крутова\n" +
+                "\n" +
+                "Иван Юрин\n" +
+                "\n" +
+                "Петр Лыков\n" +
+                "\n" +
+                "Павел Чернов\n" +
+                "\n" +
+                "Петр Чернышов\n" +
+                "\n" +
+                "Мария Федорова\n" +
+                "\n" +
+                "Марина Светлова\n" +
+                "\n" +
+                "Мария Савина\n" +
+                "\n" +
+                "Мария Рыкова\n" +
+                "\n" +
+                "Марина Лугова\n" +
+                "\n" +
+                "Анна Владимирова\n" +
+                "\n" +
+                "Иван Мечников\n" +
+                "\n" +
+                "Петр Петин\n" +
+                "\n" +
+                "Иван Ежов";
 
         phoneBook.put(61234,"Иванов");
         phoneBook.put(42234, "Петров");
@@ -15,6 +50,8 @@ public class HwFive {
         phoneBook.put(81234,"Иванов");
 
         giveMeNums(phoneBook, "Иванов" );
+        giveMyRowers(employees);
+
 
 
 
@@ -22,10 +59,31 @@ public class HwFive {
     static void giveMeNums(HashMap <Integer, String> pb, String name){
         for (var item: pb.entrySet()){
             if(item.getValue().equals(name)){
+//                System.out.println(item);
                 System.out.println(item.getKey()  + "   " + item.getValue());
 
             }
         }
+
+    }
+
+
+    static void giveMyRowers(String rowers){
+        String[] arrEmp = rowers.split("\n\n");
+        HashMap<String, Integer> listNames = new HashMap<>();
+        for (int i = 0; i <arrEmp.length ; i++) {
+            String name = arrEmp[i].split(" ")[0];
+            int countName =0;
+            for (int j = 0; j < arrEmp.length; j++) {
+                if(name.equals((arrEmp[j].split(" ")[0]))){
+                    countName++;
+                }
+            }
+            if(!listNames.containsKey(name)){
+                listNames.put(name, countName);
+            }
+        }
+        System.out.println(listNames);
 
     }
 }
--- TaskFromSeminar/S5.java
+++ TaskFromSeminar/S5.java
@@ -19,5 +19,51 @@ public class S5 {
             }
 
         }
+//        Даны 2 строки, написать метод, который вернет true, если эти строки являются изоморфными и false, если нет.
+//        Строки изоморфны, если одну букву в первом слове можно заменить на некоторую букву во втором слове, при этом
+//
+//        1. повторяющиеся буквы одного слова меняются на одну и ту же букву с сохранением порядка следования. (Например, add - egg изоморфны)
+//        2. буква может не меняться, а остаться такой же. (Например, note - code)
+//
+//        Пример 1:
+//
+//        Input: s = "foo", t = "bar"
+//
+//        Output: false
+        System.out.println(areYouIso("foooooq", "faaooob"));
+
+    }
+    static boolean areYouIso(String a, String b){
+
+        Boolean res = true;
+//        System.out.println(a.charAt(0) == b.charAt(0));
+
+        if(a.length() != b.length()){
+            System.out.println("не изоморфны");
+            return false;
+        }
+        else
+        {
+            for (int i = 0; i < (a.length() - 1); i++) {
+//                if(i <= a.length()-1){
+                    if (a.charAt(i) == a.charAt(i + 1)){
+                        if ( b.charAt(i) == b.charAt(i+1)){
+                        continue;
+                        }
+                        else
+                        {
+                            System.out.println("не изоморфны");
+                            return false;
+                        }
+                    }
+//                    else
+//                    {
+//                        continue;
+//                    }
+//                }
+            }
+        }
+        System.out.println("изоморфны");
+        return res;
     }
 }
