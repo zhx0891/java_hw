36092f408807bf1ae5c1628d8f535c1d8e87f08b
zhukovv <northbug@yandex.ru>
2023-02-25 11:44:36 +0500

    finish usrchoice

--- HW7/AutomatOfDrinks.java
+++ HW7/AutomatOfDrinks.java
@@ -2,6 +2,7 @@ package HW7;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public abstract class AutomatOfDrinks {
     ArrayList<Drink> drinks = null;
@@ -11,6 +12,22 @@ public abstract class AutomatOfDrinks {
         this.drinks = listProduct;
     }
      public void userChoice(){
+         System.out.println("Выерите продукт: \n");
+         for (int i = 0; i < this.drinks.size(); i++) {
+//             System.out.println(""+ (i + 1) + " " + this.drinks.get(i).getTradeMark() + " "
+//             + this.drinks.get(i).getVolume() + " " + this.drinks.get(i).getTemperature());
+             Drink drink = this.drinks.get(i);
+             Iterator<String> components = drink;
+             System.out.print((i+1) + " ");
+             while (components.hasNext()) {
+                 System.out.print(drink.hasNext()+ " ");
+             }
+             Scanner scan = new Scanner(System.in);
+             int usrChoice = scan.nextInt();
+             getProduct(this.drinks.get(usrChoice - 1).getTradeMark(),
+                        this.drinks.get(usrChoice - 1).getVolume(),
+                        this.drinks.get(usrChoice - 1).getTemperature());
+         }
 
 
      }
@@ -21,13 +38,13 @@ public abstract class AutomatOfDrinks {
 
              while (components.hasNext()) {
                  int count = 0;
-                 if (drink.next().equals(tm) || count == 0) {
+                 if (drink.next().equals(tm)) {
                      count++;
                  }
-                 if (drink.next().equals(Double.toString(vol)) || count == 1) {
+                 if (drink.next().equals(Double.toString(vol)) && count == 1) {
                      count++;
                  }
-                 if (drink.next().equals(Integer.toString(tmpr)) || count == 2) {
+                 if (drink.next().equals(Integer.toString(tmpr)) && count == 2) {
                      return drink;
                  }
                  else {
