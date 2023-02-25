bf0b01e0b4e5d20683285c3ff9e82706ec6048f6
zhukovv <northbug@yandex.ru>
2023-02-25 11:58:04 +0500

    begin launcher

--- HW7/AutomatHotDrinks.java
+++ HW7/AutomatHotDrinks.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 
 
 public class AutomatHotDrinks extends AutomatOfDrinks {
-    public AutomatHotDrinks(ArrayList<HotDrink> listProduct) {
-        super(listProduct);
-    }
+   public AutomatHotDrinks(ArrayList<Drink> listProduct) {
+      super(listProduct);
+   }
+//    public AutomatHotDrinks(ArrayList<HotDrink> listProduct) {
+//        super(listProduct);
+   }
 
 
-    public void getProduct(String name, double volume, int temperature) {
-
-        for (int i = 0; i < drinks.size(); i++) {
-            if (drinks.get(i).getTradeMark().equals(name)) {
-                if (drinks.get(i).getVolume() == volume) {
-                    if (drinks.get(i).getTemperature() == temperature) {
-                        drinks.get(i).showParam();
-                    }
-                }
-            }
-        }
-    }
-}
+//    public void getProduct(String name, double volume, int temperature) {
+//
+//        for (int i = 0; i < drinks.size(); i++) {
+//            if (drinks.get(i).getTradeMark().equals(name)) {
+//                if (drinks.get(i).getVolume() == volume) {
+//                    if (drinks.get(i).getTemperature() == temperature) {
+//                        drinks.get(i).showParam();
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
--- HW7/AutomatOfDrinks.java
+++ HW7/AutomatOfDrinks.java
@@ -4,30 +4,34 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
-public abstract class AutomatOfDrinks {
+
+ abstract class AutomatOfDrinks {
     ArrayList<Drink> drinks = null;
-
     public AutomatOfDrinks(ArrayList<Drink> listProduct) {
-
         this.drinks = listProduct;
     }
+
+
      public void userChoice(){
          System.out.println("Выерите продукт: \n");
          for (int i = 0; i < this.drinks.size(); i++) {
 //             System.out.println(""+ (i + 1) + " " + this.drinks.get(i).getTradeMark() + " "
 //             + this.drinks.get(i).getVolume() + " " + this.drinks.get(i).getTemperature());
+
              Drink drink = this.drinks.get(i);
              Iterator<String> components = drink;
+
              System.out.print((i+1) + " ");
              while (components.hasNext()) {
                  System.out.print(drink.hasNext()+ " ");
              }
-             Scanner scan = new Scanner(System.in);
-             int usrChoice = scan.nextInt();
-             getProduct(this.drinks.get(usrChoice - 1).getTradeMark(),
-                        this.drinks.get(usrChoice - 1).getVolume(),
-                        this.drinks.get(usrChoice - 1).getTemperature());
          }
+
+         Scanner scan = new Scanner(System.in);
+         int usrChoice = scan.nextInt();
+         getProduct(this.drinks.get(usrChoice - 1).getTradeMark(),
+                 this.drinks.get(usrChoice - 1).getVolume(),
+                 this.drinks.get(usrChoice - 1).getTemperature());
 
 
      }
@@ -46,11 +50,11 @@ public abstract class AutomatOfDrinks {
                  }
                  if (drink.next().equals(Integer.toString(tmpr)) && count == 2) {
                      return drink;
-                 }
-                 else {
+                 } else {
                      count = 0;
                  }
              }
          }
-     }
+    return null;
+    }
 }
