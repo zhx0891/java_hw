6180a49caf94696103a970d9a85b2af74fa046a7
zhukovv <northbug@yandex.ru>
2023-02-26 15:59:30 +0500

    laucher

--- HW7/AutomatOfDrinks.java
+++ HW7/AutomatOfDrinks.java
@@ -6,55 +6,55 @@ import java.util.Scanner;
 
 
  abstract class AutomatOfDrinks {
-    ArrayList<Drink> drinks = null;
-    public AutomatOfDrinks(ArrayList<Drink> listProduct) {
-        this.drinks = listProduct;
-    }
+     ArrayList<Drink> drinks = null;
 
-
-     public void userChoice(){
-         System.out.println("Выерите продукт: \n");
-         for (int i = 0; i < this.drinks.size(); i++) {
-//             System.out.println(""+ (i + 1) + " " + this.drinks.get(i).getTradeMark() + " "
-//             + this.drinks.get(i).getVolume() + " " + this.drinks.get(i).getTemperature());
-
-             Drink drink = this.drinks.get(i);
-             Iterator<String> components = drink;
-
-             System.out.print((i+1) + " ");
-             while (components.hasNext()) {
-                 System.out.print(drink.hasNext()+ " ");
-             }
-         }
-
-         Scanner scan = new Scanner(System.in);
-         int usrChoice = scan.nextInt();
-         getProduct(this.drinks.get(usrChoice - 1).getTradeMark(),
-                 this.drinks.get(usrChoice - 1).getVolume(),
-                 this.drinks.get(usrChoice - 1).getTemperature());
-
-
+     public AutomatOfDrinks(ArrayList<Drink> listProduct) {
+         this.drinks = listProduct;
      }
-     public Drink getProduct(String tm, double vol, int tmpr) {
+     public Drink getProduct (String tm,double vol, int tmpr){
          for (int i = 0; i < this.drinks.size(); i++) {
              Drink drink = this.drinks.get(i);
              Iterator<String> components = drink;
 
              while (components.hasNext()) {
                  int count = 0;
-                 if (drink.next().equals(tm)) {
+                 if (components.next().equals(tm)) {
                      count++;
                  }
-                 if (drink.next().equals(Double.toString(vol)) && count == 1) {
+                 if (components.next().equals(Double.toString(vol)) && count == 1) {
                      count++;
                  }
-                 if (drink.next().equals(Integer.toString(tmpr)) && count == 2) {
+                 if (components.next().equals(Integer.toString(tmpr)) && count == 2) {
                      return drink;
                  } else {
                      count = 0;
                  }
              }
          }
-    return null;
-    }
-}
+         return null;
+     }
+
+
+     public void userChoice() {
+         System.out.println("Выерите продукт: \n");
+         for (int i = 0; i < this.drinks.size(); i++) {
+             System.out.println("" + (i + 1) + " " + this.drinks.get(i).getTradeMark() + " "
+                     + this.drinks.get(i).getVolume() + " " + this.drinks.get(i).getTemperature());
+
+//             Drink drink = this.drinks.get(i);
+//             Iterator<String> components = drink;
+//
+//             System.out.print((i+1) + " ");
+//             while (components.hasNext()) {
+//                 System.out.print(drink.hasNext()+ " ");
+//             }
+//         }
+         }
+         Scanner scan = new Scanner(System.in);
+         int usrChoice = scan.nextInt();
+         getProduct(this.drinks.get(usrChoice - 1).getTradeMark(),
+                 this.drinks.get(usrChoice - 1).getVolume(),
+                 this.drinks.get(usrChoice - 1).getTemperature()).showParam();
+
+     }
+ }
--- HW7/Launcher.java
+++ HW7/Launcher.java
@@ -1,23 +1,24 @@
-//package HW7;
-//
-//import java.util.ArrayList;
-//
-//public class Launcher {
-//    public static void main(String[] args) {
-//        ArrayList<HotDrink> drinks = new ArrayList<>();
-//
-//        HotDrink drink1 = new HotDrink("coffee_1", 0.5 , 60);
-//        drinks.add(drink1);
-//        HotDrink drink2 = new HotDrink("coffee_1", 0.33, 40);
-//        drinks.add(drink2);
-//        HotDrink drink3 = new HotDrink("coffee_3", 0.5, 50);
-//        drinks.add(drink3);
-//        HotDrink drink4 = new HotDrink("tea", 0.5, 50);
-//        drinks.add(drink4);
-//        AutomatHotDrinks automat1 = new AutomatHotDrinks(drinks);
+package HW7;
+
+import java.util.ArrayList;
+
+public class Launcher {
+    public static void main(String[] args) {
+        ArrayList<Drink> drinks = new ArrayList<>();
+
+        HotDrink drink1 = new HotDrink("coffee_1", 0.5 , 60);
+        drinks.add(drink1);
+        HotDrink drink2 = new HotDrink("coffee_1", 0.33, 40);
+        drinks.add(drink2);
+        HotDrink drink3 = new HotDrink("coffee_3", 0.5, 50);
+        drinks.add(drink3);
+        HotDrink drink4 = new HotDrink("tea", 0.5, 50);
+        drinks.add(drink4);
+        AutomatOfDrinks automat1 = new AutomatHotDrinks(drinks);
 //        automat1.getProduct("coffee_1",0.5 , 40);
-//    }
-//}
+        automat1.userChoice();
+    }
+}
 ////    Создать наследника реализованного класса ГорячийНапиток с дополнительным
 ////    полем int температура.
 ////    Создать класс ГорячихНапитковАвтомат реализующий интерфейс ТорговыйАвтомат и
