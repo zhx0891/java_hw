3610bbdbeeef427dbb25c821aa2fd4457c5b9574
zhukovv <northbug@yandex.ru>
2023-03-02 23:24:32 +0500

    test geneircs

---
+++ HW9/Test.java
@@ -0,0 +1,11 @@
+package HW9;
+
+public class Test {
+    private int id;
+    public Test(int id){
+        this.id = id;
+    }
+    public String toString(){
+        return String.valueOf(this.id);
+    }
+}
