162c4d337f701935fe4ef50924b8bb8c82fb25c6
zhukovv <northbug@yandex.ru>
2023-03-03 16:55:25 +0500

    added classes

---
+++ HW8/Track.java
@@ -0,0 +1,12 @@
+package HW8;
+
+public class Track extends Auto{
+    public Track(String tradeMark, int weight, int speed) {
+        super(tradeMark, weight, speed);
+    }
+
+    @Override
+    void ignition() {
+
+    }
+}
