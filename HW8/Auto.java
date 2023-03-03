1a23714b414eb9d07b4964ccf2bac15d3759d61b
zhukovv <northbug@yandex.ru>
2023-03-03 16:50:34 +0500

    begin eighth hw

---
+++ HW8/Auto.java
@@ -0,0 +1,17 @@
+package HW8;
+
+abstract class Auto {
+    private String tradeMark;
+    private  int weight;
+    private int speed;
+
+    public Auto(String tradeMark, int weight, int speed) {
+        this.tradeMark = tradeMark;
+        this.weight = weight;
+        this.speed = speed;
+    }
+abstract void ignition();
+    public double move(double distance){
+        return distance / this.speed;
+    }
+}
